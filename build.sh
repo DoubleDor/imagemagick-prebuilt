c0eb0e72f93f18ed85f3ff0af0bb9e107659adac
Michael Lyons <mdl0394@gmail.com>
2016-05-17 11:12:41 -0700

    Initial Commit

---
+++ build.sh
@@ -0,0 +1,19 @@
+docker build -t imagemagick-prebuilt .
+
+mkdir artifacts
+
+docker run -i --rm -v ${PWD}/artifacts:/mnt/artifacts imagemagick-prebuilt /bin/bash << COMMANDS
+mkdir -p /mnt/artifacts/imagemagick
+cd /mnt/artifacts
+apt-get install
+wget http://www.imagemagick.org/download/ImageMagick.tar.gz
+tar xvzf ImageMagick.tar.gz
+cd ImageMagick-*
+./configure --disable-installed --prefix=/mnt/artifacts/imagemagick
+make
+make install
+echo Changing owner from \$(id -u):\$(id -g) to $(id -u):$(id -u)
+chown -R $(id -u):$(id -u) /mnt/artifacts
+COMMANDS
+
+tar -zcvf imagemagick.tar.gz /mnt/artifacts/imagemagick
