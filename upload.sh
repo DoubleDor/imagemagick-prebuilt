24be63d664ebad805561eb5176f7028c32591945
Michael Lyons <mdl0394@gmail.com>
2016-05-17 13:22:48 -0700

    Fixed install.sh

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 git tag "v${1}"
 git push --tags
 github-release release --tag "v${1}" --name "ImageMagick Prebuilt v${1}" --description "Prebuilt image of ImageMagick for AWS Lambda v${1}"
-github-release upload --tag "v${1}" --name "imagemagic.tar.gz" --file artifacts/imagemagick-prebuilt.tar.gz
+github-release upload --tag "v${1}" --name "imagemagick-prebuilt.tar.gz" --file artifacts/imagemagick-prebuilt.tar.gz
